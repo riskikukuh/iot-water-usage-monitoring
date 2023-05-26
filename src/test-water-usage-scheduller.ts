b9809ffe7daee2b5469090efacc82022e871692e
riskikukuh <riskikukuh.dev@gmail.com>
2023-05-26 14:23:24 +0700

    fix dummy water usage scheduller

--- src/test-water-usage-scheduller.ts
+++ src/test-water-usage-scheduller.ts
@@ -2,10 +2,15 @@ import * as schedule from "node-schedule"
 import { AppDataSource } from "./data-source"
 import { create } from "./services/WaterUsageService"
 import * as dotenv from "dotenv"
+import axios from "axios"
+import { resolve } from "path/posix"
+import { response } from "express"
 dotenv.config()
 
 AppDataSource.initialize().then(async () => {
 
+    const host = process.env.HOST
+    const port = parseInt(process.env.PORT)
     const userIdTesting = '9d0a806d-0fd5-4558-bedf-f9e5a660a246'
     const unit = 'liter/m'
     
@@ -20,16 +25,36 @@ AppDataSource.initialize().then(async () => {
     let minute = now.getMinutes()
 
     hour = 0
-    minute = 0
+    minute = 0   
 
     schedule.scheduleJob('* * * * *', async function() {
         counter += 1
 
-        const endDate = new Date()
-        const startDate = new Date(endDate.getTime() - 60000)
-        const usage = Math.floor(Math.random() * 100)
+        const requestBody = {
+            user_id: "9d0a806d-0fd5-4558-bedf-f9e5a660a246",
+            usage: Math.floor(Math.random() * (130 - 0) + 0),
+            unit: "liter/m",
+            usage_at: +new Date(),
+        }
 
-        const id = await create(userIdTesting, usage, +endDate, unit)
-        console.log(`Success make water usage with id: ${id}`)
+        try {
+            const { data, status } = await axios.post(`http://${host}:${port}/api/waterUsage`, requestBody, {
+                headers: {
+                  'Content-Type': 'application/json',
+                  Accept: 'application/json',
+                },
+            })
+            if (status == 201) {
+                console.log(`# Add Water usage result ${status}, with id: ${data.data.id}`)
+            } else {
+                console.log(`# Add Water Usage error`)
+                console.log(AppDataSource)
+                console.log(status)
+            }
+            
+            
+        } catch (error) {
+            console.log(`Unexpected error: ${error}`)
+        }
     })
 })
