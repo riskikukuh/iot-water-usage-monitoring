bfae9a6ce1e44938da5fd3f307de0e5338237bac
riskikukuh <riskikukuh.dev@gmail.com>
2023-06-01 14:06:10 +0700

    add bill-scheduler

---
+++ src/bill-scheduller.ts
@@ -0,0 +1,34 @@
+import * as schedule from "node-schedule"
+import { AppDataSource } from "./data-source"
+import { create } from "./services/HistoryService"
+import { getAllUser } from "./services/UserService"
+
+AppDataSource.initialize().then(async () => {
+    console.log("Start Aggregate Service")
+    
+    // const userIdTesting = '9d0a806d-0fd5-4558-bedf-f9e5a660a246'
+
+    // schedule.scheduleJob('0 0 L * *', async function() {
+    schedule.scheduleJob('* * * * *', async function() {
+
+        const now = new Date()
+        now.toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
+        const day = now.getDate()
+        const month = now.getMonth()
+        const year = now.getFullYear()
+
+        const allUser = await getAllUser(true)
+        for (let i = 0; i < allUser.length; i++) {
+            const user = allUser[i]
+            const { id } = user
+
+            const startDate = +new Date(year, month, day - 1, 0, 0, 0)
+            const endDate = +new Date(year, month, day, 0, 0, 0)
+
+            console.log(`# Aggregating usage for 1 day, start: ${+startDate} end: ${+endDate}`)
+
+            const aggregateId = await create(id, startDate, endDate)
+            console.log(`Success make aggregate with id: ${aggregateId}`)
+        }
+    })
+})
