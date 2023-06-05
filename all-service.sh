d731be940972b8e34cd534b7be4257d91b4df9d1
riskikukuh <riskikukuh.dev@gmail.com>
2023-06-05 22:41:42 +0700

    fix name

--- all-service.sh
+++ all-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pm2 start --name "dummy-water-usage-service" npm -- run water-usage:debug
-pm2 start --name "histories-service" npm -- run history-scheduller
-pm2 start --name "bill-service" npm -- run bill-service:debug
+pm2 start --name "histories-service" npm -- run histories-service
+pm2 start --name "bill-service" npm -- run bill-service
